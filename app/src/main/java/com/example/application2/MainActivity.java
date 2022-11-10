5a72010c96da30d5b6c85e60715943a7fdf6a94f
DELL <bellhossain12@gmail.com>
2022-11-10 16:44:09 +0600

    First Test

---
+++ app/src/main/java/com/example/application2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.application2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.firstandroidapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/firstandroidapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.firstandroidapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity  {
+
+    private WebView web1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+}
